1254590e8a9115b728ef74639a58dd039c1e695d
Ana Riccetti <anariccetti@gmail.com>
2018-10-29 21:18:08 -0300

    created roberto coach

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,3 @@ Rails.application.routes.draw do
   get 'ask', to: 'questions#ask', as: :ask
   post 'answer', to: 'questions#answer', as: :answer
 end
-
-# Prefix           Verb URI Pattern                 Controller#Action
-# questions_ask    GET  /questions/ask(.:format)    questions#ask
-# questions_answer GET  /questions/answer(.:format) questions#answer
-
-# Rails.application.routes.draw do
-#   get 'about', to: 'pages#about', as: :about
-#   get 'contact', to: 'pages#contact', as: :contact
-# end
-
-# Prefix        Verb  URI Pattern         Controller#Action
-# pages_about   GET   /about(.:format)    pages#about
-# pages_contact GET   /contact(.:format)  pages#contact
